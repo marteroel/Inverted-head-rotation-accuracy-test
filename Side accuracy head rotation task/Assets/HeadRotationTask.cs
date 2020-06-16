7a4f6dd245e18bc83ac2fc53365c7d9211e9c375
marteroel <marteroel@users.noreply.github.com>
2020-06-16 14:27:59 +0200

    now logs data and uses headset input

--- Side accuracy head rotation task/Assets/HeadRotationTask.cs
+++ Side accuracy head rotation task/Assets/HeadRotationTask.cs
@@ -2,63 +2,92 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HeadRotationTask : MonoBehaviour
-{
+public class HeadRotationTask : MonoBehaviour {
     
 	public Transform head;
-	public bool activateRandomly;
-	public AudioSource sound;
-    public int lMinThres, lMaxThres, rMinThres, rMaxThres;
+    public AudioSource sound;
+    public bool activateRandomly;
+    public int totalTrials;   
+    [Tooltip("left min, left max, right min, right max")]
+    public int[] bounds = new int[4];
+    public bool inverted;//TODO
 
 	private bool isInRange;
 	private string lastDirection;
+    private Timer _timer;
 
+    private string[] mouseResponse = new string[] {"left", "right"};
+    private int count;
 
 	void Start() {
 		string[] directions = new string[] {"left", "right"};
 		lastDirection = directions[Random.Range(0,2)];
+
+        _timer = Timer.instance;
 	}
 
-    void Update()
-    {
-		if(!isInRange){
-            
-			if(lastDirection == "right")
-				if(head.eulerAngles.y < lMaxThres && head.eulerAngles.y > lMinThres)
-					StartCoroutine(Trial("left")); 
+    void Update() {
+
+        if (count < totalTrials){
+     
+		    if(!isInRange){          
+			    if(lastDirection == "right")//last direction is used so that only when entering the range coming from the center
+				    if(head.eulerAngles.y > bounds[0] && head.eulerAngles.y < bounds[1])
+					    StartCoroutine(Trial("left")); 
 			
-			if(lastDirection == "left") 
-				if(head.eulerAngles.y < rMaxThres && head.eulerAngles.y > rMinThres)
-					StartCoroutine(Trial("right"));			
-		}	
+			    if(lastDirection == "left") 
+				    if(head.eulerAngles.y > bounds[2] && head.eulerAngles.y < bounds[3])
+					    StartCoroutine(Trial("right"));			
+		    }
+        }
+
+        if (Input.GetKeyDown("space"))//space restarts count
+            count = 0;
     }
 
-	public IEnumerator Trial(string side){
+	public IEnumerator Trial(string side) {
        
         int trialOrNot;
+        string answer = null;
 
         if (activateRandomly) trialOrNot = Random.Range(0, 2);
         else trialOrNot = 1;
 
-        isInRange = true;
-		//start timer
+        isInRange = true;    
 
 		if(trialOrNot == 1) {
 			sound.Play();
+            _timer.stopwatch.Start();
 
-			if(side == "left") 
-				while(head.eulerAngles.y < lMaxThres && head.eulerAngles.y > lMinThres)
-					yield return null;	
-			else  
-				while(head.eulerAngles.y < rMaxThres  && head.eulerAngles.y > rMinThres)
-					yield return null;
+            while (!Input.anyKeyDown) //while there is no key pressed, wait.
+                yield return null;
+
+            if (Input.GetMouseButton(0)) //if the pressed key is left
+                if (side == mouseResponse[0]) answer = "correct";
+                else answer = "incorrect";
+
+            else if (Input.GetMouseButton(1))//if the pressed key is right
+                if (side == mouseResponse[1]) answer = "correct";
+                else answer = "incorrect";
+
+            string[] variables = new string[] { count.ToString(), inverted.ToString(), side, _timer.ElapsedTimeAndRestart(), answer };
+
+            CsvWrite.instance.WriteLine(variables);//writes trial to file
+            Debug.Log(string.Join(",",variables));
+
+            while (Input.anyKey) //waits while key is still pressed before proceeding
+                yield return null;
+            
+            count++;
 		}
 
-		else yield return null;
-		
+		else yield return null;//so not each head turn is a trial
+
+        if (count == totalTrials)
+            Debug.Log("condition is over");
+
 		lastDirection = side;
-		isInRange = false;
-		
+		isInRange = false;		
 	}
 
 }
