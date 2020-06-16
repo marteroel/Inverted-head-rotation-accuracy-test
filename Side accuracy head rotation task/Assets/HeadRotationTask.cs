8080f26f062fabc007e43ed6bfa6800ba4927ef3
marteroel <marteroel@users.noreply.github.com>
2020-06-16 11:01:28 +0200

    cleaned script

--- Side accuracy head rotation task/Assets/HeadRotationTask.cs
+++ Side accuracy head rotation task/Assets/HeadRotationTask.cs
@@ -6,8 +6,10 @@ public class HeadRotationTask : MonoBehaviour
 {
     
 	public Transform head;
-	public bool alwaysActive;
+	public bool activateRandomly;
 	public AudioSource sound;
+    public int lMinThres, lMaxThres, rMinThres, rMaxThres;
+
 
 	private bool isInRange;
 	private string lastDirection;
@@ -15,57 +17,50 @@ public class HeadRotationTask : MonoBehaviour
 
 	void Start() {
 		string[] directions = new string[] {"left", "right"};
-		lastDirection = directions[Random.Range(0,2)]; //initializing lastDirection so whichever side can be first
-		Debug.Log("initial direction " + lastDirection);
+		lastDirection = directions[Random.Range(0,2)]; //initializing lastDirection randomly so either side can be first
 	}
 
     // Update is called once per frame
     void Update()
     {
 		if(!isInRange){
-			
+            
 			if(lastDirection == "right")
-				if(head.eulerAngles.y < 90 && head.eulerAngles.y > 45)
+				if(head.eulerAngles.y < lMaxThres && head.eulerAngles.y > lMinThres)
 					StartCoroutine(Trial("left")); 
 			
-			if(lastDirection == "left") {
-				if(head.eulerAngles.y > 270 && head.eulerAngles.y < 315){
-					Debug.Log("left");
-					StartCoroutine(Trial("right"));
-					}
-			}
-		}
-		
+			if(lastDirection == "left") 
+				if(head.eulerAngles.y < rMaxThres && head.eulerAngles.y > rMinThres)
+					StartCoroutine(Trial("right"));			
+		}	
     }
 
 	public IEnumerator Trial(string side){
-		
-		int trialOrNot = Random.Range(0,2);
-		if(alwaysActive) trialOrNot = 1;
+       
+        int trialOrNot;
 
-		isInRange = true;
+        if (activateRandomly) trialOrNot = Random.Range(0, 2);
+        else trialOrNot = 1;
+
+        isInRange = true;
 		//start timer
 
 		if(trialOrNot == 1) {
-			//Debug.Log("happen");
-			Debug.Log("start " + side );
-			//sound.Play();
+		    //Debug.Log("start " + side );
+			sound.Play();
 
 			if(side == "left") 
-				while(head.eulerAngles.y < 90 && head.eulerAngles.y > 45 )
+				while(head.eulerAngles.y < lMaxThres && head.eulerAngles.y > lMinThres)
 					yield return null;	
 			else  
-				while(head.eulerAngles.y > 270 && head.eulerAngles.y < 315 )
-					yield return null;	
+				while(head.eulerAngles.y < rMaxThres  && head.eulerAngles.y > rMinThres)
+					yield return null;
 
-			Debug.Log("stop " + side);
+            //Debug.Log("stop " + side);
 		}
 
-		else {
-			Debug.Log("not happen");
-			yield return null;
-		}
-
+		else yield return null;
+		
 		lastDirection = side;
 		isInRange = false;
 		
